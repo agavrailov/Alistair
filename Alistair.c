b44e59ec2998d43a3770f85d75a14147fb918bbe
agavrailov <agavrailov@gmail.com>
2020-01-31 22:18:14 +0200

    Fixed bug priceHigh in SellAlgo

--- Alistair.c
+++ Alistair.c
@@ -1,19 +1,15 @@
 #include <default.c> 							//трябва да го има заради другите #include команди
+// #include "Strategy\include\objective.h"
 static bool b1,b2,b3,b4,s1,s2,s3,s4	=	false;	//трябва да стои извън if(is(INITRUN))
 
-	// TMF that adjusts the stop in a special way 
-int TrailingStop() {
-	
-	// adjust the stop only when the trade is in profit  if(TradeProfit > 0)// place the stop at the lowest bottom of the previous 3 candles
-    TradeStopLimit = max(TradeStopLimit,LL(3));
-	
-	// plot a line to make the stop limit visible in the chart 
-	plot("Stop",TradeStopLimit,MINV,BLACK);
-// return 0 for checking the limits  return 0;
+int TrailingStop() { // TMF that adjusts the stop in a special way 
+	if(TradeProfit > 0)	// adjust the stop only when the trade is in profit  
+		TradeStopLimit = max(TradeStopLimit,LL(3));	// place the stop at the lowest bottom of the previous 3 candles
+	plot("Stop",TradeStopLimit,MINV,BLACK);	// plot a line to make the stop limit visible in the chart 
+	return 0;	// return 0 for checking the limits  
 }
-
 	
-function BuyAlgo(var EMA_H, EMA_L, TimePeriod, tr)	// Buy signal
+function BuyAlgo(var EMA_H, EMA_L, TimePeriod, tp, tr)	// Buy signal
 {
 	if (price() < LL(TimePeriod,1))	b1 = true;	//пресичане на най-ниската стойност
 	if (price() > HH(TimePeriod,1))	b1 = false;	//пресичане на най-високата стойност
@@ -38,13 +34,14 @@ function SellAlgo(var EMA_H, EMA_L, TimePeriod, tp, tr)	// Buy signal
 	if (price() < LL(TimePeriod,1))	s1 = false;	//пресичане на най-високата стойност
 	if (s1 and !s3 and (priceOpen() > EMA_H)) {	//затворен бар под на MA5(Low)
 		s2 = true;
-		Stop = priceLow(1);						//стопа е там където е b1
+		Stop = priceHigh(1);						//стопа е там където е b1
 	}
 	if (s2 and (priceOpen() < EMA_L))	s3 = true;	//затворен бар над на MA5(High)
 	if (s3 and priceClose() > EMA_H)	s4 = true;	//затворен бар под на MA5(Low)
 	if (s4) {		//всички условия налице
 		enterShort(TrailingStop);
-		s2=s3=s4 = false;	
+		s2=s3=s4 = false;
+		stop1	=	priceHigh(1)
 	}
 }
 function run() 
@@ -58,7 +55,6 @@ function run()
 	MonteCarlo		=	0;
 	StartDate		=	2017;
 	EndDate			=	2019;
-	
 	BarPeriod 		=	240;
 	LookBack 		=	1000; 
 	var TimePeriod	=	200; //optimize (1000,100,1400,100);
@@ -68,7 +64,7 @@ function run()
 	
 	var EMA_H 	= EMA(series(priceHigh()),5);
 	var EMA_L 	= EMA(series(priceLow()),5);
-	// TakeProfit 		= 	tp*ATR(20); 
+	TakeProfit 		= 	tp*ATR(20); 
 	Trail 			=	tr*ATR(20); 
 	// Capital 		=	4000;
 	// Margin 		=	0.2 * OptimalF * Capital * sqrt(1 + (WinTotal-LossTotal)/Capital);
@@ -79,6 +75,6 @@ function run()
 				 case 	"SELL"	: SellAlgo(EMA_H, EMA_L, TimePeriod, tp, tr);
 			}
 	}
-#include "Strategy\include\plot.h"
-// #include "Strategy\include\objective.h"
+#include "Strategy\include\plot.h"		//чертае сигналите
+
 }
