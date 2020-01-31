8660671b9c78dad4c43fc94411f2034f7d0bc537
agavrailov <agavrailov@gmail.com>
2020-01-31 16:08:15 +0200

    започвам да оптимизирам настройки

--- Alistair.c
+++ Alistair.c
@@ -37,16 +37,16 @@ function run()
 		if(is(TRAINMODE)) set(PARAMETERS+FACTORS);
 		NumCores	=	-1;
 	}
-	var TimePeriod	=	1000; 	// optimize (1000,100,1500,100);
 	MonteCarlo		=	0;
 	StartDate		=	2016;
 	EndDate			=	2020;
-	BarPeriod 		=	240;
+	BarPeriod 		=	60;
 	LookBack 		=	1500; 
+	var TimePeriod	=	optimize (1000,100,1500,100);
 	var tp			=	3; //optimize(1,0,3,0.2);	//TakeProfit
 	var tr 			=	0.1; //optimize(0,0,1,0.1);			//Trail
 	var sl 			=	0; 		//optimize(10,1,20,1);	//Допълнително отстояние от най-ниската цена в периода
-	// NumWFOCycles 	=	3 * (EndDate-StartDate);
+	NumWFOCycles 	=	3 * (EndDate-StartDate);
 	
 	var EMA_H = EMA(series(priceHigh()),5);
 	var EMA_L = EMA(series(priceLow()),5);
