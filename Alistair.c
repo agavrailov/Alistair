e52b934b47fa27dfb06280ad81a0d4477c766ba3
agavrailov <agavrailov@gmail.com>
2020-01-31 20:18:57 +0200

    TMF за управление на стопа на сделката.

--- Alistair.c
+++ Alistair.c
@@ -1,31 +1,49 @@
+#include <default.c> 							//трябва да го има заради другите #include команди
 static bool b1,b2,b3,b4,s1,s2,s3,s4	=	false;	//трябва да стои извън if(is(INITRUN))
-function BuyAlgo(var EMA_H, EMA_L, TimePeriod, tp, tr, sl)	// Buy signal
+
+	// TMF that adjusts the stop in a special way 
+int TrailingStop() {
+	
+	// adjust the stop only when the trade is in profit  if(TradeProfit > 0)// place the stop at the lowest bottom of the previous 3 candles
+    TradeStopLimit = max(TradeStopLimit,LL(3));
+	
+	// plot a line to make the stop limit visible in the chart 
+	plot("Stop",TradeStopLimit,MINV,BLACK);
+// return 0 for checking the limits  return 0;
+}
+
+	
+function BuyAlgo(var EMA_H, EMA_L, TimePeriod, tr)	// Buy signal
 {
 	if (price() < LL(TimePeriod,1))	b1 = true;	//пресичане на най-ниската стойност
 	if (price() > HH(TimePeriod,1))	b1 = false;	//пресичане на най-високата стойност
 	if (b1 and !b3 and (priceOpen() < EMA_L)) {	//затворен бар под на MA5(Low)
 		b2 = true;
-		Stop = priceLow(1) - sl*PIP;	//стопа е там където е b1 + 10 пипа
+		Stop = priceLow(1);						//стопа е там където е b1
 	}
 	if (b2 and (priceOpen() > EMA_H))	b3 = true;	//затворен бар над на MA5(High)
 	if (b3 and priceClose() < EMA_L)	b4 = true;	//затворен бар под на MA5(Low)
 	if (b4) {		//всички условия налице
-		enterLong();
+		enterLong(TrailingStop);
 		b2=b3=b4 = false;	
 	}
 }
-function SellAlgo(var EMA_H, EMA_L, TimePeriod, tp, tr, sl)	// Buy signal
+function SellAlgo(var EMA_H, EMA_L, TimePeriod, tp, tr)	// Buy signal
 {
+// close longs first, if any
+// var RSI		= crossUnder(series(RSI(series(price()),14)),30);
+
+//open shorts	
 	if (price() > HH(TimePeriod,1))	s1 = true;	//пресичане на най-ниската стойност
 	if (price() < LL(TimePeriod,1))	s1 = false;	//пресичане на най-високата стойност
 	if (s1 and !s3 and (priceOpen() > EMA_H)) {	//затворен бар под на MA5(Low)
 		s2 = true;
-		Stop = priceLow(1) + sl*PIP;	//стопа е там където е b1 + 10 пипа
+		Stop = priceLow(1);						//стопа е там където е b1
 	}
 	if (s2 and (priceOpen() < EMA_L))	s3 = true;	//затворен бар над на MA5(High)
 	if (s3 and priceClose() > EMA_H)	s4 = true;	//затворен бар под на MA5(Low)
 	if (s4) {		//всички условия налице
-		enterShort();
+		enterShort(TrailingStop);
 		s2=s3=s4 = false;	
 	}
 }
@@ -40,25 +58,27 @@ function run()
 	MonteCarlo		=	0;
 	StartDate		=	2017;
 	EndDate			=	2019;
-	BarPeriod 		=	15;
-	LookBack 		=	1500; 
-	var TimePeriod	=	optimize (1000,100,1400,100);
-	var tp			=	optimize(1,0,2,0.2);	//TakeProfit
-	var tr 			=	optimize(0,0,1,0.1);			//Trail
-	var sl 			=	0; 		//optimize(10,1,20,1);	//Допълнително отстояние от най-ниската цена в периода
-	NumWFOCycles 	=	2 * (EndDate-StartDate);
 	
-	var EMA_H = EMA(series(priceHigh()),5);
-	var EMA_L = EMA(series(priceLow()),5);
-	TakeProfit 		= 	tp*ATR(20); 
+	BarPeriod 		=	240;
+	LookBack 		=	1000; 
+	var TimePeriod	=	200; //optimize (1000,100,1400,100);
+	var tp			=	optimize(1,0,2,0.2);	//TakeProfit
+	var tr 			=	optimize(1,0,1,0.1);			//Trail
+	// NumWFOCycles 	=	3 * (EndDate-StartDate);
+	
+	var EMA_H 	= EMA(series(priceHigh()),5);
+	var EMA_L 	= EMA(series(priceLow()),5);
+	// TakeProfit 		= 	tp*ATR(20); 
 	Trail 			=	tr*ATR(20); 
 	// Capital 		=	4000;
 	// Margin 		=	0.2 * OptimalF * Capital * sqrt(1 + (WinTotal-LossTotal)/Capital);
 	while(asset(loop(Assets))) {
 		while(algo(loop("BUY","SELL")))
 			switch (Algo) {
-				case	"BUY"	: BuyAlgo(EMA_H, EMA_L, TimePeriod, tp, tr, sl);
-				case 	"SELL"	: SellAlgo(EMA_H, EMA_L, TimePeriod, tp, tr, sl);
+				// case	"BUY"	: BuyAlgo(EMA_H, EMA_L, TimePeriod, tp, tr);
+				 case 	"SELL"	: SellAlgo(EMA_H, EMA_L, TimePeriod, tp, tr);
 			}
 	}
+#include "Strategy\include\plot.h"
+// #include "Strategy\include\objective.h"
 }
---
+++ include/plot.h
@@ -0,0 +1,11 @@
+	// PlotHeight2 = 30;
+	// PlotScale = 10;
+	setf(PlotMode,PL_BENCHMARK);
+	// plot("EMA_L", EMA_L, LINE, YELLOW);
+	// plot("EMA_H", EMA_H, LINE, MAGENTA);
+	plot("HH", HH(TimePeriod,1), LINE, BLACK);
+	plot("Стоп", Stop, LINE, GREY);
+	plot("c1", s1, NEW|LINE, GREY);
+	plot("c2", s2, NEW|LINE, GREY);
+	plot("c3", s3, NEW|LINE, GREY);
+	plot("c4", s4, NEW|LINE, GREY);
